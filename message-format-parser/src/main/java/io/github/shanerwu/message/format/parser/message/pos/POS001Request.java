cd898c5c96af36ce5d3148630397635155ecaae3
Winni <yes585yes585@gmail.com>
2023-02-18 10:03:02 +0800

    POS001換成兼容原版AbstractMessageSupport規格版本

--- message-format-parser/src/main/java/io/github/shanerwu/message/format/parser/message/pos/POS001Request.java
+++ message-format-parser/src/main/java/io/github/shanerwu/message/format/parser/message/pos/POS001Request.java
@@ -4,7 +4,7 @@ import io.github.shanerwu.message.format.core.annotation.MessageDetail;
 import io.github.shanerwu.message.format.parser.message.AbstractMessageSupport;
 
 @MessageDetail("POS001Request")
-public class POS001Request extends AbstractPOSRequest<POS001RequestBody> {
+public class POS001Request extends AbstractMessageSupport<POSReqHeader, POS001RequestBody> {
 
     public POS001Request(String message) {
         super(message);
