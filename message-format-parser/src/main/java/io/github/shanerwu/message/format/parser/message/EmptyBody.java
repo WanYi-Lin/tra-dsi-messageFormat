8f727ae0d2235e4d379aaa905b491e18b0d6ded4
Winni <yes585yes585@gmail.com>
2023-02-20 13:54:04 +0800

    [ 修正 ]錯誤電文解析失敗異常:res改塞EmptyBody [ 修正 ]電文解析工具顯示的錯誤訊息不明確之異常

---
+++ message-format-parser/src/main/java/io/github/shanerwu/message/format/parser/message/EmptyBody.java
@@ -0,0 +1,9 @@
+package io.github.shanerwu.message.format.parser.message;
+
+import io.github.shanerwu.message.format.core.MessageFormatSupport;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class EmptyBody extends MessageFormatSupport {
+
+}
